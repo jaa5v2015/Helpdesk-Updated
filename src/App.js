5871770c90024578a2b1386386e42d7756182430
Jacob A. Anderson <BH05230@tn.gov>
2022-06-27 10:30:56 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -3,132 +3,134 @@ import Button from "@mui/material/Button"
 import Stack from "@mui/material/Stack"
 import Dashboard from "./components/Dashboard";
 import DateSelector from "./components/DateSelector"
+import axios from 'axios'
 
 
 
 
+export default class App extends React.Component {
 
-export default class App extends React.Component{
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      dataChoice: "Total",
+      activeData: [],
+      activeEmployees: ["TWRA Intern (BH05115)", "Tom Wochna (BH01558)", "Zack Dover (BH05212)", "Jay Ghussein (BH05196)"],
+      tickets: [],
+      date: new Date(),
+      filterDate: "",
+      employeeList: [],
+      mounted: false,
+
+    }
+  }
+
+  async componentDidMount() {
+    const e = []
+    var data = []
+    let isMounted = true
     
-      constructor(props){
-        super(props);
-        this.state = {
-          data: [],
-          dataChoice: "Total",
-          activeData: [],
-          activeEmployees: ["TWRA Intern (BH05115)", "Tom Wochna (BH01558)", "Zack Dover (BH05212)", "Jay Ghussein (BH05196)"],
-          tickets: [],
-          date: new Date(),
-          filterDate: "",
-          employeeList: [],
-          mounted: false,
-          
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        console.log("Getting data")
+        this.setState({ data: data })
+
+      }
+    )
+    this.state.activeEmployees.map(activeEmp => {
+      this.state.data.map(allEmp => {
+        if (allEmp.name == activeEmp) {
+          e.push(allEmp)
         }
-      }
+      })
+    })
+    this.setState({ activeData: e })
+    let ticketList = []
 
-      componentDidMount(){
-        const e = []
-        var data = []
-        let isMounted=true
-        fetch("/members").then(
-          res => res.json()
-        ).then(
-          data=> {
-            this.setState({data:data})
-            
-          }
-        )
-        this.state.activeEmployees.map(activeEmp=>{
-          this.state.data.map(allEmp =>{
-            if(allEmp.name == activeEmp){
-              e.push(allEmp)
-            }
-          })
-        })
-        this.setState({activeData: e})
-        let ticketList = []
-        
-        this.state.data.map(employee =>{
-          
-            employee.tickets.map(ticket =>{
-               ticketList.push(ticket)
-               
-               var date = new Date(ticket.timeCreated)
-                ticket.timeCreated = date;
-           
-                
-            })
-    
-    
-           
-        })
-       
-       this.setState({tickets: ticketList})
+    this.state.data.map(employee => {
 
-        
-        let filter = ticketList.filter( ticket =>ticket.timeCreated < this.state.date && ticket.timeCreated > this.state.filterDate)
+      employee.tickets.map(ticket => {
+        ticketList.push(ticket)
 
-       
-      }
+        var date = new Date(ticket.timeCreated)
+        ticket.timeCreated = date;
 
-      componentWillUnmount(){
-        this.setState = (state,callback) =>{
-          return;
-        }
-      }
 
-      setFromDate = (date) =>{
-        this.setState({filterDate: date})
-        
-      }
-
-      setToDate = (date) =>{
-        this.setState({date: date})
-      }
-
-      changeData = (e) =>{
-        this.setState({dataChoice: e})
-        
-      }
-
-    
+      })
 
 
 
-      render(){
+    })
 
-        return (
-        
-          <div className="App">
-          
- 
-       <Stack direction="row" spacing={2}>
-       
-       <Button variant="contained"  onClick={()=> this.changeData("Total")} >Total Incidents</Button>
-          <Button variant="contained" onClick={()=> this.changeData("Access")}  >Access Incident</Button>
-          <Button variant="contained" onClick={()=> this.changeData("Help")} >Help / Assistance Incidents</Button>
-          <Button variant="contained"  onClick={()=> this.changeData("Fail")}  >Failure Incidents</Button>
-         <Button variant="contained"  onClick={()=> this.changeData("Incidents")}  >View Incidents</Button>
-         <DateSelector filterDate={this.setFromDate} label={"From"} />
+    this.setState({ tickets: ticketList })
+
+
+    let filter = ticketList.filter(ticket => ticket.timeCreated < this.state.date && ticket.timeCreated > this.state.filterDate)
+
+
+  }
+
+  componentWillUnmount() {
+    this.setState = (state, callback) => {
+      return;
+    }
+  }
+
+  setFromDate = (date) => {
+    this.setState({ filterDate: date })
+
+  }
+
+  setToDate = (date) => {
+    this.setState({ date: date })
+  }
+
+  changeData = (e) => {
+    this.setState({ dataChoice: e })
+
+  }
+
+
+
+
+
+  render() {
+
+    return (
+
+      <div className="App">
+
+
+        <Stack direction="row" spacing={2}>
+
+          <Button variant="contained" onClick={() => this.changeData("Total")} >Total Incidents</Button>
+          <Button variant="contained" onClick={() => this.changeData("Access")}  >Access Incident</Button>
+          <Button variant="contained" onClick={() => this.changeData("Help")} >Help / Assistance Incidents</Button>
+          <Button variant="contained" onClick={() => this.changeData("Fail")}  >Failure Incidents</Button>
+          <Button variant="contained" onClick={() => this.changeData("Incidents")}  >View Incidents</Button>
+          <DateSelector filterDate={this.setFromDate} label={"From"} />
           <p3> ____</p3>
-         <DateSelector filterDate={this.setToDate} label={"To"} />
-       </Stack>
- 
-       <div className="App2">
-                
-               <Dashboard mounted={this.state.mounted} data={this.state.data} dataChoice={this.state.dataChoice} filterDate={this.state.filterDate} date={this.state.date}/>
-             
-             
-       </div>
-     </div>
- 
-   
-   );
+          <DateSelector filterDate={this.setToDate} label={"To"} />
+        </Stack>
+
+        <div className="App2">
+
+          <Dashboard mounted={this.state.mounted} data={this.state.data} dataChoice={this.state.dataChoice} filterDate={this.state.filterDate} date={this.state.date} />
+
+
+        </div>
+      </div>
+
+
+    );
 
 
 
 
-      }
+  }
 
 }
--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import { useState } from "react"
 import Graph from "./graph"
 import "./style/table.css"
 import { BiInfoCircle } from "react-icons/bi";
@@ -6,17 +6,17 @@ import Incident from "./incident";
 import Popup from "./popup";
 
 
-const  Table = props =>{
-    
+const Table = props => {
+
     const [showData, toggleData] = useState(false);
-    const toggleInfo= (employee) =>{
+    const toggleInfo = (employee) => {
         toggleData(!showData)
         console.log(employee)
     }
 
-    if(props.dataChoice == "Total"){
-        return(
-            <div style={{display:"flex", flexDirection:'row', width:"100%", overflowY:"scroll",height:"95%"}}>
+    if (props.dataChoice == "Total") {
+        return (
+            <div style={{ display: "flex", flexDirection: 'row', width: "100%", overflowY: "scroll", height: "95%" }}>
                 <div className="table-container">
                     <h1>Total Incident Report</h1>
                     <div className="table">
@@ -24,133 +24,133 @@ const  Table = props =>{
                         <h3>Name</h3>
                         <h3>Total Incidents</h3>
                         <h3>Avg Time</h3>
+                    </div>
+                    {
+
+                        props.data.map(employee => {
+
+                            return (
+                                <div className="table">
+                                    <div onClick={() => toggleInfo(employee)}> <BiInfoCircle /> {showData && <Popup data={employee.tickets} />} </div>
+                                    <h3>{employee.name}</h3>
+                                    <h3>{employee.Incidents}</h3>
+                                    <h3>{(employee.time / employee.Incidents).toFixed(2)} seconds</h3>
+
+                                </div>
+                            )
+                        })
+                    }
+
+
                 </div>
-                {
-                   
-                    props.data.map(employee =>{
-                        
-                        return(
-                            <div className="table">
-                                <div onClick={() => toggleInfo(employee)}> <BiInfoCircle/> {showData && <Popup data={employee.tickets} />} </div>
-                                <h3>{employee.name}</h3>
-                                <h3>{employee.Incidents}</h3>
-                                <h3>{(employee.time / employee.Incidents).toFixed(2)} seconds</h3>
-                                
-                            </div>
-                        )
-                    })
-                }
 
-                
-            </div>
-                
             </div>
         );
-    
+
     }
 
-    else if(props.dataChoice=="Access"){
-        return(
-            <div style={{display:"flex", flexDirection:'row', overflowY:"scroll",height:"90%"}}>
-            <div className="table-container">
-                <h1>Access Incident Report</h1>
-                <div className="table">
-                    <h3>Name</h3>
-                    <h3>Access Incidents</h3>
-                    <h3>Avg Time</h3>
-            </div>
-            {
-                
-                props.data.map(employee =>{
-                    var avg = 0
-                    if(employee.AccessIncidents != 0){
-                        avg = employee.AccessTime / employee.AccessIncidents
-                    }
-                    return(
-                        <div className="table">
-                            <h3>{employee.name}</h3>
-                            <h3>{employee.AccessIncidents}</h3>
-                            <h3>{avg.toFixed(2)} seconds</h3>
-                            
-                        </div>
-                    )
-                })
-            }
+    else if (props.dataChoice == "Access") {
+        return (
+            <div style={{ display: "flex", flexDirection: 'row', overflowY: "scroll", height: "90%" }}>
+                <div className="table-container">
+                    <h1>Access Incident Report</h1>
+                    <div className="table">
+                        <h3>Name</h3>
+                        <h3>Access Incidents</h3>
+                        <h3>Avg Time</h3>
+                    </div>
+                    {
 
-            
-        </div>
-            
-        </div>
+                        props.data.map(employee => {
+                            var avg = 0
+                            if (employee.AccessIncidents != 0) {
+                                avg = employee.AccessTime / employee.AccessIncidents
+                            }
+                            return (
+                                <div className="table">
+                                    <h3>{employee.name}</h3>
+                                    <h3>{employee.AccessIncidents}</h3>
+                                    <h3>{avg.toFixed(2)} seconds</h3>
+
+                                </div>
+                            )
+                        })
+                    }
+
+
+                </div>
+
+            </div>
         );
     }
 
-    else if(props.dataChoice=="Help"){
-        return(
-            <div style={{display:"flex", flexDirection:'row', width:"100%",overflowY:"scroll",height:"90%"}}>
-            <div className="table-container">
-                <h1>Help Incident Report</h1>
-                <div className="table">
-                    <h3>Name</h3>
-                    <h3>Help / Assistance Incidents</h3>
-                    <h3>Avg Time</h3>
-            </div>
-            {
-                
-                props.data.map(employee =>{
-                    var avg = 0
-                    if(employee.HelpIncidents != 0){
-                        avg = employee.helpTime / employee.HelpIncidents
-                    }
-                    return(
-                        <div className="table">
-                            <h3>{employee.name}</h3>
-                            <h3>{employee.HelpIncidents}</h3>
-                            <h3>{avg.toFixed(2)} seconds</h3>
-                            
-                        </div>
-                    )
-                })
-            }
+    else if (props.dataChoice == "Help") {
+        return (
+            <div style={{ display: "flex", flexDirection: 'row', width: "100%", overflowY: "scroll", height: "90%" }}>
+                <div className="table-container">
+                    <h1>Help Incident Report</h1>
+                    <div className="table">
+                        <h3>Name</h3>
+                        <h3>Help / Assistance Incidents</h3>
+                        <h3>Avg Time</h3>
+                    </div>
+                    {
 
-            
-        </div>
-            
-        </div>
+                        props.data.map(employee => {
+                            var avg = 0
+                            if (employee.HelpIncidents != 0) {
+                                avg = employee.helpTime / employee.HelpIncidents
+                            }
+                            return (
+                                <div className="table">
+                                    <h3>{employee.name}</h3>
+                                    <h3>{employee.HelpIncidents}</h3>
+                                    <h3>{avg.toFixed(2)} seconds</h3>
+
+                                </div>
+                            )
+                        })
+                    }
+
+
+                </div>
+
+            </div>
         );
     }
 
-    else if(props.dataChoice=="Fail"){
-        return(
-            <div  style={{display:"flex", flexDirection:'row', width:"100%",overflowY:"scroll",height:"90%"}} >
-            <div className="table-container">
-                <h1>Failure Incident Report</h1>
-                <div className="table">
-                    <h3>Name</h3>
-                    <h3>Failure Incidents</h3>
-                    <h3>Avg Time</h3>
-            </div>
-            {
-                
-                props.data.map(employee =>{
-                    var avg = 0
-                    if(employee.FailCount != 0){
-                        avg = employee.FailTime / employee.FailCount
-                    }
-                    return(
-                        <div className="table">
-                            <h3>{employee.name}</h3>
-                            <h3>{employee.FailCount}</h3>
-                            <h3>{avg.toFixed(2)} seconds</h3>
-                            
-                        </div>
-                    )
-                })
-            }
+    else if (props.dataChoice == "Fail") {
+        return (
+            <div style={{ display: "flex", flexDirection: 'row', width: "100%", overflowY: "scroll", height: "90%" }} >
+                <div className="table-container">
+                    <h1>Failure Incident Report</h1>
+                    <div className="table">
+                        <h3>Name</h3>
+                        <h3>Failure Incidents</h3>
+                        <h3>Avg Time</h3>
+                    </div>
+                    {
 
-            
-        </div>
-            
-        </div>
+                        props.data.map(employee => {
+                            var avg = 0
+                            if (employee.FailCount != 0) {
+                                avg = employee.FailTime / employee.FailCount
+                            }
+                            return (
+                                <div className="table">
+                                    <h3>{employee.name}</h3>
+                                    <h3>{employee.FailCount}</h3>
+                                    <h3>{avg.toFixed(2)} seconds</h3>
+
+                                </div>
+                            )
+                        })
+                    }
+
+
+                </div>
+
+            </div>
         );
     }
 
